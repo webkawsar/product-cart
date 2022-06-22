13384241b72ba2bae8749aa4aecdbd8216b21957
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-06-22 13:38:33 +0600

    added edit and update feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@
     const priceInputElm = document.querySelector('.product-price');
     const listGroupElm = document.querySelector('.list-group');
     const filterElm = document.querySelector('#filter');
+    const addProductElm = document.querySelector('.add-product');
 
 
     // data layer
@@ -33,6 +34,7 @@
         const htmlListElm = `<li class="list-group-item item-${id} collection-item">
                                 <strong>${name}</strong>- <span class="price">$${price}</span>
                                 <i class="fa fa-trash delete-item float-right"></i>
+                                <i class="fa fa-pencil-alt edit-item float-right me-4"></i>
                             </li>`;
         listGroupElm.insertAdjacentHTML('afterbegin', htmlListElm);
     }
@@ -62,6 +64,7 @@
             const htmlListElm = `<li class="list-group-item item-${item.id} collection-item">
                                     <strong>${item.name}</strong>- <span class="price">$${item.price}</span>
                                     <i class="fa fa-trash delete-item float-right"></i>
+                                    <i class="fa fa-pencil-alt edit-item float-right me-4"></i>
                                 </li>`;
             listGroupElm.insertAdjacentHTML('afterbegin', htmlListElm);
 
@@ -98,9 +101,27 @@
         localStorage.setItem('storeProducts', JSON.stringify(products));
     }
 
+    const populateUIInState = (product) => {
+        nameInputElm.value = product.name;
+        priceInputElm.value = product.price;
+    }
+
+    const showUpdateBtn = () => {
+        const elm = `<button type="button" class="btn mt-3 btn-block btn-secondary update-product">Update</button>`;
+        addProductElm.style.display = 'none';
+        formElm.insertAdjacentHTML('beforeend', elm);
+    }
+
+    const updateProductsToStorage = () => {
+
+        if(localStorage.getItem('storeProducts')) {
+            localStorage.setItem('storeProducts', JSON.stringify(products));
+        }
+    }
 
 
     const init = () => {
+        let updatedItemId;
             
         formElm.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -115,14 +136,9 @@
                 return;
             }
 
-
             // generate id
             let id = products.length;
-            const storeProducts = localStorage.getItem('storeProducts')
-            if(storeProducts) {
-                id = JSON.parse(storeProducts).length;
-            }
-            
+                        
             // add item to data store
             const product = {
                 id,
@@ -141,11 +157,52 @@
 
         })
 
+        formElm.addEventListener('click', e => {
+            if(e.target.classList.contains('update-product')) {
+                // pick the data fom the field
+                const {nameInput, priceInput} = receiveInputs();
+
+                // validate input
+                const isError = validateInput(nameInput, priceInput);
+                if(isError) {
+                    alert('Please provide valid input');
+                    return;
+                }
+
+                // updated data in temporary data store
+                products = products.map(product => {
+                    if(product.id === updatedItemId) {
+                        return {
+                            id: product.id,
+                            name: nameInput,
+                            price: priceInput
+                        }
+                    } else {
+                        return product;
+                    }
+                })
+
+                // reset input
+                resetInput();
+
+                // hide update button
+                document.querySelector('.update-product').remove();
+
+                // show add button
+                addProductElm.style.display = 'block';
+
+                // update data in UI
+                showAllItemsToUI(products);
+
+                // update data in local storage
+                updateProductsToStorage();
+            }
+        })
+
         listGroupElm.addEventListener('click', (e) => {
             if(e.target.classList.contains('delete-item')) {
         
                 const id = getItemId(e.target);
-        
                 // delete item from UI
                 removeItemFromUI(id);
         
@@ -154,6 +211,22 @@
 
                 // delete product from local storage
                 removeFromLocalStorage(id);
+
+            } else if(e.target.classList.contains('edit-item')) {
+                // pick the item id
+                updatedItemId = getItemId(e.target);
+
+                // find the item
+                const foundProduct = products.find(item => item.id === updatedItemId);
+
+                // populate data and show input
+                populateUIInState(foundProduct);
+                
+                // show update button
+                if(!document.querySelector('.update-product')) {
+                    showUpdateBtn();
+                }
+                
             }
         })
 
@@ -170,8 +243,9 @@
             // checking item into local storage
             const storeProducts = localStorage.getItem('storeProducts');
             if(storeProducts) {
-                const products = JSON.parse(storeProducts);
+                products = JSON.parse(storeProducts);
                 showAllItemsToUI(products);
+
             }
         })
         
